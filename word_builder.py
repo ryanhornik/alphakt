2c40d19fd5bc244e3777f0251f814b600c362779
Ryan Hornik <ryan.hornik@towbee.com>
2016-04-21 09:26:19 -0500

    Created functions to prune the word list, and split it into even chunks

--- word_builder.py
+++ word_builder.py
@@ -19,21 +19,18 @@ def save_dataset(path, word_counts):
 
 
 def get_words(path, word_counts):
-    with open(path) as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            put_words(clean_words(row['title'].split()), 3, word_counts)
-            put_words(clean_words(row['selftext'].split()), 1, word_counts)
-
-
-def put_words(words, weight, word_counts):
-    for word in words:
-        word_counts[word] = word_counts.get(word, 0) + weight
+    csvfile = open(path)
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        for word in clean_words(row['title'].lower().split()):
+            word_counts[word] = word_counts.get(word, 0) + 3
+        for word in clean_words(row['selftext'].lower().split()):
+            word_counts[word] = word_counts.get(word, 0) + 1
 
 
 def clean_words(words):
     alphanumeric = re.compile(r'[^0-9a-z]+')
-    return map(lambda word: alphanumeric.sub('', word), words)
+    return filter(None, map(lambda word: alphanumeric.sub('', word), words))
 
 
 def main(path):
@@ -42,8 +39,31 @@ def main(path):
     save_dataset(path, word_counts)
 
 
+def prune(path):
+    file = open(path)
+    outfile = open(path[:-4] + "_pruned" + path[-4:], mode='w')
+    reader = csv.reader(file)
+    writer = csv.writer(outfile)
+
+    for row in reader:
+        if int(row[1]) < 500 or len(row[0]) <= 3 or int(row[1]) > 6000:
+            continue
+        writer.writerow(row)
+
+
+def split(path, n):
+    file = open(path)
+    outfiles = [open(path[:-4] + "_split_" + str(x) + path[-4:], mode='w') for x in range(1, n + 1)]
+    reader = csv.reader(file)
+    writers = [csv.writer(f) for f in outfiles]
+
+    for i, row in enumerate(reader):
+        writers[i % n].writerow(row)
+
+
 if __name__ == "__main__":
     from sys import argv
+
     if len(argv) >= 2:
         main(argv[1])
     else:

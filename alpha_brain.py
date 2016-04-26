cbf3a452e888948e0ff7af5bc590f8bfd4ed8e3b
Ryan Hornik <ryan.hornik@towbee.com>
2016-04-26 13:05:56 -0500

    experiments with training the nn

--- alpha_brain.py
+++ alpha_brain.py
@@ -3,31 +3,41 @@ import csv
 import os
 
 from pybrain.datasets import SupervisedDataSet
-from pybrain.structure import FeedForwardNetwork, FullConnection, LinearLayer, SigmoidLayer
+from pybrain.tools.shortcuts import buildNetwork
+
 from constants import input_columns
 from pybrain.supervised import BackpropTrainer
 
 
+def check(nn, csv_in):
+    for row in csv_in:
+        inputs = tuple(map(lambda x: row[x], input_columns))
+        if '' in inputs or row['8hr_score'] == '':
+            continue
+        print('Guessed: {}\nActual: {}'.format(nn.activate(inputs), row['8hr_score']))
+
+
 def build_nn():
     print("Building nn")
-    nn = FeedForwardNetwork()
-
-    # 24 columns per dataset  with bins 34
-    in_layer = LinearLayer(len(input_columns))
-    hidden_layer = SigmoidLayer(12)
-    out_layer = LinearLayer(1)
-
-    nn.addInputModule(in_layer)
-    nn.addModule(hidden_layer)
-    nn.addOutputModule(out_layer)
-
-    in_to_hidden = FullConnection(in_layer, hidden_layer)
-    hidden_to_out = FullConnection(hidden_layer, out_layer)
-
-    nn.addConnection(in_to_hidden)
-    nn.addConnection(hidden_to_out)
-
-    nn.sortModules()
+    nn = buildNetwork(len(input_columns), 12, 1)
+    # nn = FeedForwardNetwork()
+    #
+    # # 24 columns per dataset  with bins 34
+    # in_layer = LinearLayer(len(input_columns))
+    # hidden_layer = SigmoidLayer(12)
+    # out_layer = LinearLayer(1)
+    #
+    # nn.addInputModule(in_layer)
+    # nn.addModule(hidden_layer)
+    # nn.addOutputModule(out_layer)
+    #
+    # in_to_hidden = FullConnection(in_layer, hidden_layer)
+    # hidden_to_out = FullConnection(hidden_layer, out_layer)
+    #
+    # nn.addConnection(in_to_hidden)
+    # nn.addConnection(hidden_to_out)
+    #
+    # nn.sortModules()
 
     return nn
 
@@ -51,6 +61,13 @@ def build_data(directory):
     return ds
 
 
+def get_all(directory):
+    nn = build_nn()
+    ds = build_data(directory)
+    trainer = BackpropTrainer(nn, ds)
+    return nn, ds, trainer
+
+
 def main():
     nn = build_nn()
     ds = build_data('completed_data/fixed/')
@@ -62,4 +79,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
--- constants.py
+++ constants.py
@@ -2,16 +2,16 @@ from os import environ
 from os.path import expanduser
 
 HOME = expanduser('~/.alphakt')
-# reddit_secret = environ['REDDIT_SECRET']
-# client_id = environ['REDDIT_CLIENT_ID']
+reddit_secret = environ['REDDIT_SECRET']
+client_id = environ['REDDIT_CLIENT_ID']
 redirect_uri = "http://127.0.0.1:65010/authorize_callback"
 POSTS_PER_MINUTE = 500  # This is way beyond my estimate but it worked for my test of 10 to 15 min
-#
-# access_credentials = {
-#     'access_token': environ['ACCESS_TOKEN'],
-#     'refresh_token': environ['REFRESH_TOKEN'],
-#     'scope': {'read'}
-# }
+
+access_credentials = {
+    'access_token': environ['ACCESS_TOKEN'],
+    'refresh_token': environ['REFRESH_TOKEN'],
+    'scope': {'read'}
+}
 
 
 child_directories = (
